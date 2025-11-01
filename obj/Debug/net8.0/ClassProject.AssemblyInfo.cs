a76b33237857cd5111ca78af72f5ef93babbeca6
faiza-code <f2iza.na20@gmail.com>
2025-11-01 19:59:43 +0400

    add new file

--- obj/Debug/net8.0/ClassProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClassProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21303d01591b65080f50d7c2eedcb547fdc625c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e755e7c76bd5f6e517c7867043a38953154b3b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
